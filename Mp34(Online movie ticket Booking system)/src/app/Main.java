89fbc75fa4be425d68aa254cae9e4889f27d56cd
awhaleyash7478 <awhaleyash7478@gmail.com>
2026-07-04 07:04:42 +0530

    added customer verification feature

---
+++ Mp34(Online movie ticket Booking system)/src/app/Main.java
@@ -0,0 +1,33 @@
+package app;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+import  services.*;
+public class Main {
+    public static void main(String[]args)
+    {
+        
+         Scanner sc=new Scanner(System.in);
+            
+            try 
+            {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localHost:3306/demodatabase",
+                    "root",
+                    "Yash@7478");
+                    
+            System.out.println("connection established successfully");
+            CustomerVerification l=new CustomerVerification(sc,conn);
+            l.menu();
+            
+                
+              
+    }catch(SQLException e)
+    {
+        e.printStackTrace();
+    }
+}
+
+}
